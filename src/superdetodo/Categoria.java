faf30187000f6d683b887f3374db16282db7b92e
sebastian castro <95104254+ssebasss@users.noreply.github.com>
2023-08-31 15:41:47 -0300

    first commit

---
+++ src/superdetodo/Categoria.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package superdetodo;
+
+import java.awt.PointerInfo;
+
+/**
+ *
+ * @author Seba
+ */
+public enum Categoria {
+    
+    COMESTIBLE,
+    LIMPIEZA,
+    PERFUMERIA,
+    
+    
+    
+}
---
+++ src/superdetodo/Producto.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package superdetodo;
+
+public class Producto implements Comparable<Producto>{
+    private int codigo;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    private Categoria rubro;
+
+    public Producto() {
+    }
+
+    public Producto(int codigo, String descripcion, double precio, int stock, Categoria rubro) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.rubro = rubro;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Categoria getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(Categoria rubro) {
+        this.rubro = rubro;
+    }
+
+    @Override
+    public int compareTo(Producto o) {//la idea es comparar este producto con otro q mandan x parametro
+           if(codigo==o.codigo) {
+            return 0;
+    } else if(codigo>o.codigo){
+        return 1;
+    }else{
+       return -1;  
+    }
+}
+}
